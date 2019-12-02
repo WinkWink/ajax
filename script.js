7116672efef0ee39d795b9867cdacfaa7706ab9b
WinkWink <amanda.winkler44@gmail.com>
2019-12-02 15:35:57 -0500

    updated looping

--- script.js
+++ script.js
@@ -3,13 +3,13 @@
 document.getElementById('ajax-fruit').onsubmit = function() { 
     var e = document.getElementById("fruit");
     var fruitChosen = e.options[e.selectedIndex].value;
+    
     $.getJSON('http://amandawinkler.space/fruits.json',function(data){
-    var appleName = data[0]['name'];
-    if (appleName === fruitChosen) {
-        console.log('you choose and app');
-    }else{
-        console.log('you choose not the right fruit u dumbo')
-    }
+        function makeGraph (fruit){
+            console.log(data[fruit]['name']);
+        }
+
+        makeGraph(fruitChosen);
 });
     return false;
 
