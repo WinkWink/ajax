6686f0c34c737e44e3c863891c0592fb4010c3fd
WinkWink <amanda.winkler44@gmail.com>
2019-12-01 21:27:51 -0500

    this has turned into a non ajax project real quick

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-$.ajax({
-  dataType: "json",
-  url: 'http://amandawinkler.space/fruits.json',
-  data: data,
-  success: success
+
+
+document.getElementById('ajax-fruit').onsubmit = function() { 
+    var e = document.getElementById("fruit");
+    var fruitChosen = e.options[e.selectedIndex].value;
+    $.getJSON('http://amandawinkler.space/fruits.json',function(data){
+    var appleName = data[0]['name'];
+    if (appleName === fruitChosen) {
+        console.log('you choose and app');
+    }else{
+        console.log('you choose not the right fruit u dumbo')
+    }
 });
+    return false;
+
+};
